0e5bbe63a847244ba98d6fb215b92917484c8f2b
fadi abuaita <77901233+fadiabuaita-star@users.noreply.github.com>
2021-08-15 15:17:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
+
+namespace MongoDB.NET
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BsonClassMap.RegisterClassMap<NameModel>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+            });
+
+            MongoCRUD db = new MongoCRUD("AddressBook");
+
+            var recs = db.LoadRecords<NameModel>("Users");
+
+            System.Console.WriteLine("done program");
+
+            foreach (var rec in recs)
+            {
+                System.Console.WriteLine($"{rec.Id} {rec.FirstName} {rec.LastName}");
+
+                System.Console.WriteLine();
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
